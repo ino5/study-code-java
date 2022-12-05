873d1680703c0656d797d390190d8955df450137
ino5 <chero77@naver.com>
2022-12-05 23:11:19 +0900

    그동안 백준 푼 거 커밋

---
+++ src/baekjoon/P10986_221129.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/10986
+// 나머지합
+// 골드3
+public class P10986_221129 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] nums = new long[N];
+        long[] sums = new long[N];
+        long[] rcnts = new long[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
+        }
+
+        sums[0] = nums[0] % M;
+        rcnts[Long.valueOf(sums[0]).intValue()]++;
+        for (int i = 1; i < sums.length; i++) {
+            sums[i] = sums[i - 1] + (nums[i] % M);
+            sums[i] %= M;
+            rcnts[Long.valueOf(sums[i]).intValue()]++;
+        }
+
+        int answer = 0;
+        answer += rcnts[0];
+        for (int i = 0; i < rcnts.length; i++) {
+            if (rcnts[i] > 1) {
+                answer += (rcnts[i] * (rcnts[i] - 1)) / 2;
+            }
+        }
+        System.out.println(answer);
+    }
+}
---
+++ src/baekjoon/P11660/Main_221128.java
@@ -0,0 +1,39 @@
+package baekjoon.P11660;
+
+import java.util.*;
+
+public class Main_221128 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] map = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] sumMap = new int[N + 1][N + 1]; // (0,0)부터의 합
+        sumMap[1][1] = map[1][1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sumMap[i][j] = sumMap[i][j - 1] + sumMap[i - 1][j]
+                             - sumMap[i - 1][j - 1] +map[i][j];
+            }
+        }
+
+        // 정답 구하기
+        for (int M_i = 0; M_i < M; M_i++) {
+            int a_i = sc.nextInt();
+            int a_j = sc.nextInt();
+            int b_i = sc.nextInt();
+            int b_j = sc.nextInt();
+            int answer = sumMap[b_i][b_j]
+                       - sumMap[a_i - 1][b_j]
+                       - sumMap[b_i][a_j - 1]
+                       + sumMap[a_i - 1][a_j - 1];
+            System.out.println(answer);
+        }
+    }
+}
